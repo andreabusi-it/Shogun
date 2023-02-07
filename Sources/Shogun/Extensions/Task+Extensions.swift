34dbc7a14523da77bf9e499fe5a0db07e2f8d6f6
Andrea Busi <andrea@andreabusi.it>
2023-02-07 21:25:47 +0100

    feat: Add extension for Task

---
+++ Sources/Shogun/Extensions/Task+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Task+Extensions.swift
+//  
+//
+//  Created by Andrea Busi on 07/02/23.
+//
+
+import Foundation
+
+
+@available(macOS 10.15, *)
+extension Task where Success == Never, Failure == Never {
+    
+    /// Suspends the current task for at least the given duration in seconds.
+    ///
+    /// Credits: https://www.hackingwithswift.com/quick-start/concurrency/how-to-make-a-task-sleep
+    public static func sleep(seconds: Double) async throws {
+        let duration = UInt64(seconds * 1_000_000_000)
+        try await Task.sleep(nanoseconds: duration)
+    }
+}
