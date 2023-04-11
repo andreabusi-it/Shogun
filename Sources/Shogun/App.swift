c322d0f2b7a3014e3818bd12746fe0310af22c98
Andrea Busi <andrea@andreabusi.it>
2023-04-11 17:11:00 +0200

    fix: Make property static

--- Sources/Shogun/App.swift
+++ Sources/Shogun/App.swift
@@ -11,7 +11,7 @@ import Foundation
 final public class App {
     
     /// Defines if the current app is running inside Xcode Previews (SwiftUI canvas)
-    public var isRunningInPreviews: Bool {
+    public static var isRunningInPreviews: Bool {
         ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
     }
 }
